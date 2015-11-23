3f247008ad921836eb47f7ef9d482acf599cf24f
ITSPaul <powell.paul@itsligo.ie>
2015-11-23 10:36:04 +0000

    MultiHub V2 added for simple message passing solution as serialisation appeared to affect performance

--- cgMonoGameServer2015/MultiHubV2.cs
+++ cgMonoGameServer2015/MultiHubV2.cs
@@ -8,37 +8,84 @@ using System.Threading.Tasks;
 
 namespace cgMonoGameServer2015
 {
-    public static class UserHandler
-    {
-        public static HashSet<string> ConnectedIds = new HashSet<string>();
-        public static HashSet<PlayerDataObject> Players = new HashSet<PlayerDataObject>();
 
+    public static class Player
+    {
+        public static string PlayerID;
+    }
+
+    public static class HubState
+    {
+        public static HashSet<string> playerConnections = new HashSet<string>();
+        public static string[] BarrierNames = new string[] {"","" };
+        public static Dictionary<string, string> playerBArriers = new Dictionary<string, string>();
     }
 
     public class MultiHubV2 : Hub
     {
-        public override Task OnConnected()
+        // When a player joins if there are any other players then inform them of a new opponent
+        public void join(string Joined, string CharacterName, int x, int y)
         {
-            UserHandler.ConnectedIds.Add(Context.ConnectionId);
-            return base.OnConnected();
+            HubState.playerConnections.Add(Context.ConnectionId);
+            Clients.All.opponentJoined(Joined, CharacterName,x,y);
+            int playerCount = HubState.playerConnections.ToList().FindIndex(p => p.Equals(Context.ConnectionId));
+            Clients.Caller.playerNumber(playerCount);
+            Clients.Others.opponentNumber(playerCount);
+        }
+        
+        // The original player that started needs to be added as an opponent as when instantiated
+        // There was no other clients to be added to so we catch up and start the game
+        public void addMe(string playerID, string CharacterName, int x, int y)
+        {
+            int playerCount = HubState.playerConnections.ToList().FindIndex(p => p.Equals(playerID));
+            Clients.Others.opponentJoined(playerID, CharacterName, x, y);
+            Clients.Others.opponentNumber(playerCount);
+            Clients.All.setup();
         }
 
-        public override Task OnDisconnected(bool stopCalled)
+        //
+        public void changeConnection(string OldConnection)
         {
-            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
-            UserHandler.Players.RemoveWhere(p => p.ConnectionClientID == Context.ConnectionId);
-            return base.OnDisconnected(stopCalled);
+            HubState.playerConnections.Remove(OldConnection);
+            HubState.playerConnections.Add(Context.ConnectionId);
         }
 
-        public void join(PlayerDataObject p)
+        // When a player leaves the game let the other clients know
+        public void leave()
         {
-            Clients.All.Joined(p, p.ConnectionClientID);
+            Clients.All.opponentLeft(Context.ConnectionId);
+            Clients.Others.Remove(Context.ConnectionId);
+            HubState.playerConnections.Remove(Context.ConnectionId);
+            
         }
 
-        public int getClients()
+        // When a player moves let the other clients know
+        public void playerMoved(int x, int y)
         {
-            return UserHandler.ConnectedIds.Count();
+            Clients.Others.opponentMoved( x,y );
         }
+
+        public void setUpOpponentBarrier(int x, int y)
+        {
+            Clients.Others.createOpponentBarrier(x, y);
+        }
+
+        public int getPlayerCount()
+        {
+            return HubState.playerConnections.Count();
+       }
+
+        public int getPlayerNumber(string ConnID)
+        {
+            return HubState.playerConnections.ToList().FindIndex(p => p.Equals(ConnID));
+        }
+
+
+        public void setUpOpponentCollectable(int x, int y)
+        {
+            Clients.Others.createOpponentCollectable(x, y);
+        }
+
 
     }
 }
