29aee524fe94d9d59088d82c9dbf03f0f0a2679a
ITSPaul <powell.paul@itsligo.ie>
2015-11-23 20:22:11 +0000

    Added Data Classes Project and class file as link in Hub Project

---
+++ DataClasses/DataClasses.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace DataClasses
+{
+    [Serializable]
+    public class GameDataObject
+    {
+        public string ConnectionClientID;
+    }
+
+    [Serializable]
+    public class PlayerDataObject : GameDataObject
+    {
+        public Vector2 position;
+        public int score;
+        public int health;
+        public int lives;
+        public string textureName;
+        public List<Collectable> collected = new List<Collectable>();
+    }
+
+    [Serializable]
+    public class Collectable : GameDataObject
+    {
+        public Vector2 position;
+        public int value;
+    }
+
+    [Serializable]
+    public class Barrier: GameDataObject
+    {
+        public Vector2 position;
+        public int strength;
+    }
+
+
+}
