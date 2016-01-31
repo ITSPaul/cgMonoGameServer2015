5457e4597bce016fe64232be1958613e29225a1c
ITSPaul <powell.paul@itsligo.ie>
2016-01-31 23:24:57 +0000

    adding AzController for Synchronisation

---
+++ cgMonoGameServer2015/Controllers/aZControlController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace cgMonoGameServer2015.Controllers
+{
+    [RoutePrefix("api/aZControl")]
+    public class aZControlController : ApiController
+    {
+        [Route("getDate")]
+        public DateTime getDate()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
